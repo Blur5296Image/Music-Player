2a7fdd79095d9ac5ccf3eb272d65c9913ae2e4c8
Blur5296Image <65455057+Blur5296Image@users.noreply.github.com>
2022-05-10 20:09:06 +0530

    Add files via upload
    music player

---
+++ Project BTTI/View/js/music-list.js
@@ -0,0 +1,80 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Heat Waves",
+    artist: "Glass Animals",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Manzar Hai Ye Naya",
+    artist: "Shantanu Sudame",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Chaand Baaliyan",
+    artist: "Aditya A",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Aaj Sajeya",
+    artist: "Goldie Sohel",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Mere Sapnon Ki Rani",
+    artist: "S.D. Burman",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Kali Kali Zulfon Ke Phande",
+    artist: "Nusrat Fateh Ali Khan",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Prada",
+    artist: "Jass Manak",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Yeh Dosti",
+    artist: "Kishore Kumar",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Sanam Re",
+    artist: "Arijit Singh",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Gulabi Ankhein",
+    artist: "Mohammed Rafi",
+    img: "music-10",
+    src: "music-10"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
